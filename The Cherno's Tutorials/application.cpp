406416638f628124160cf5fa3cdca7b64f412e40
Aaditya <ajoil4068@gmail.com>
2024-07-17 14:34:27 +0530

    Pyramid

--- The Cherno's Tutorials/application.cpp
+++ The Cherno's Tutorials/application.cpp
@@ -23,7 +23,7 @@ int main()
 	if(!glfwInit()){ return -1; }
 
 	//Create a Windowed mode Window and its OpenGL Context
-	my_Window = glfwCreateWindow(640, 640, "Abstraction of Elements", NULL, NULL);
+	my_Window = glfwCreateWindow(640, 640, "Pyramid!!!", NULL, NULL);
 	if(!my_Window){ glfwTerminate(); return -1; }
 
    	// Make the window's context current
@@ -33,14 +33,16 @@ int main()
 
 	if(glewInit() != GLEW_OK){ std::cout << "Error!" << std::endl; return -1;  }
 
+    GLCall(glEnable(GL_DEPTH_TEST));
+
     {
         
         float positions[] = {
-            0.0f,  0.0f,  0.5f,
-            1.0f,  1.0f, -0.5f,
-           -1.0f,  1.0f, -0.5f,
-           -1.0f, -1.0f, -0.5f,
-            1.0f, -1.0f, -0.5f,
+            0.0f,  0.5f,  0.0f,     // Apex
+            1.0f, -0.5f,  1.0f,     // Base vertices
+           -1.0f, -0.5f,  1.0f,
+           -1.0f, -0.5f, -1.0f,
+            1.0f, -0.5f, -1.0f,
         };
 
         unsigned int indices[] = {
@@ -49,23 +51,23 @@ int main()
             0, 3, 4,
             0, 4, 1,
             1, 2, 3,
-            2, 3, 4
+            1, 3, 4
         };
 
-        vertex_Buffer vb1(positions, 5 * 3 * sizeof(float));
-        index_Buffer ibo1(indices, 6 * 3);
+        vertex_Buffer VB1(positions, 5 * 3 * sizeof(float));
+        index_Buffer IBO1(indices, 6 * 3);
 
-        vertex_Buffer_Layout vbl1;
-        vbl1.push<float>(3);
+        vertex_Buffer_Layout VBL1;
+        VBL1.push<float>(3);
 
-        vertex_Array va1;
-        va1.add_buffer(vb1, vbl1);
+        vertex_Array VA1;
+        VA1.add_buffer(VB1, VBL1);
 
-        shader sh1("res/shaders/notsobasic.shader");
-        sh1.bind();
-        sh1.set_uniform_4f("u_Colour", 1.0f, 1.0f, 0.0f, 0.7f);
+        shader SH1("res/shaders/rotator.shader");
+        SH1.bind();
+        SH1.set_uniform_4f("u_Colour", 1.0f, 1.0f, 0.0f, 0.7f);
 
-        glm::vec3 camPos_Vector = glm::vec3(0.0f, 0.0f, 3.0f);
+        glm::vec3 camPos_Vector = glm::vec3(0.0f, 2.0f, 4.0f);
         glm::vec3 target_Vector = glm::vec3(0.0f, 0.0f, 0.0f);
         glm::vec3 up_Vector = glm::vec3(0.0f, 1.0f, 0.0f);
 
@@ -73,19 +75,27 @@ int main()
         glm::mat4 view = glm::lookAt(camPos_Vector, target_Vector, up_Vector);
         glm::mat4 projection = glm::perspective(glm::radians(45.0f), 640.0f / 640.0f, 0.1f, 100.0f);
 
-        sh1.set_uniform_mat_4f("u_Model", model, false);
-        sh1.set_uniform_mat_4f("u_View", view, false);
-        sh1.set_uniform_mat_4f("u_Projection", projection, false);
+        SH1.set_uniform_mat_4f("u_Model", model, false);
+        SH1.set_uniform_mat_4f("u_View", view, false);
+        SH1.set_uniform_mat_4f("u_Projection", projection, false);
+
+        glm::mat4 transformation = glm::mat4(1.0f);
+        SH1.set_uniform_mat_4f("u_Rotation", transformation, false);
+
+        SH1.unbind();
+
 
         while(!glfwWindowShouldClose(my_Window)){
 		    //Render here
 	    	GLCall(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
+            
+            IBO1.bind();
 
-            sh1.bind();
-            sh1.set_uniform_4f("u_Colour", 1.0f, 1.0f, 0.0f, 0.7f);
-            sh1.set_uniform_mat_4f("u_Model", model, false);
-            sh1.set_uniform_mat_4f("u_View", view, false);
-            sh1.set_uniform_mat_4f("u_Projection", projection, false);
+            transformation = glm::rotate(transformation, -0.05f, glm::vec3(0.0f, 0.1f, 0.0f));
+
+            SH1.set_uniform_mat_4f("u_Rotation", transformation, false);
+
+            SH1.bind();
 		    GLCall(glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, nullptr));
 
             // Do something here
@@ -96,7 +106,6 @@ int main()
     	    // Poll for and process events
             glfwPollEvents();
         }
-
     }
 
     glfwTerminate();
--- The Cherno's Tutorials/res/shaders/rotator.shader
+++ The Cherno's Tutorials/res/shaders/rotator.shader
@@ -5,9 +5,10 @@ layout(location = 0) in vec4 position;
 uniform mat4 u_Model;
 uniform mat4 u_View;
 uniform mat4 u_Projection;
+uniform mat4 u_Rotation;
 
 void main(){
-	gl_Position = u_Projection * u_View * u_Model * vec4(position.x, position.y, position.z, 1.0);
+	gl_Position = u_Projection * u_View * u_Rotation * u_Model * vec4(position.x, position.y, position.z, 1.0);
 }
 
 
